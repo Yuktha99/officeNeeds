ecd28f50d6b32897eacb0066df34535ad4d31098
Yuktha P <pyuktha.99@gmail.com>
2025-01-07 15:01:56 -0600

    Navigation setup

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import AppNavigator from './src/AppNavigator'
+
+const App = () => {
+  return (
+    <AppNavigator/>
+  )
+}
+
+export default App
